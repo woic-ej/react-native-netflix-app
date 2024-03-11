df7cd5f9b58bb0c55edc28ba5116be5e31616ac8
woic-ej <woic0102@naver.com>
2024-03-11 16:54:18 +0900

    first commit

---
+++ components/AddWatchListButton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AddWatchListButton = () => {
+  return (
+    <View>
+      <Text>AddWatchListButton</Text>
+    </View>
+  );
+};
+
+export default AddWatchListButton;
+
+const styles = StyleSheet.create({});
---
+++ components/Genre.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Genre = () => {
+  return (
+    <View>
+      <Text>Genre</Text>
+    </View>
+  );
+};
+
+export default Genre;
+
+const styles = StyleSheet.create({});
